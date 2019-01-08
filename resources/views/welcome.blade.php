16209150435804e4e51bb97021a02063173ffbc8
tsendayush <fondnessfear@gmail.com>
2019-01-08 15:55:34 +0800

    first day commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mercury 2.0
                 </div>
 
                 <div class="links">
